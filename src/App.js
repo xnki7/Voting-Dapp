62e88871987bb234e2239c862677f3b37d78cd03
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-05-07 21:12:10 +0530

    basic login page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import {ethers} from 'ethers';
+import { contractAbi, contractAddress } from './constant/constant';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Login/>
     </div>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login(props) {
+  return (
+    <div className='login-container'>
+        <h1 className='welcome-message'>Welcome to decentralized voting application</h1>
+        <button className="login-button" onClick = {props.connectWallet}>Login Metamask 🦊</button>
+    </div>
+  )
+}
+
+export default Login
