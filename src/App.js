05811ac39f578afba6bcd59575a5cc0bb30e7680
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-05-07 21:31:32 +0530

    connections with metamask

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import {ethers} from 'ethers';
-import { contractAbi, contractAddress } from './constant/constant';
-import Login from './components/Login';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { contractAbi, contractAddress } from "./constant/constant";
+import Login from "./components/Login.jsx";
+import Connected from "./components/Connected";
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  async function connectToMetamask() {
+    if (window.ethereum) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        console.log("Metamask Connected " + address);
+        setIsConnected(true);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      console.log("Metamask not detected in browser");
+    }
+  }
+
   return (
     <div className="App">
-      <Login/>
+      {isConnected ? (
+        <Connected account={account} />
+      ) : (
+        <Login connectWallet={connectToMetamask} />
+      )}
     </div>
   );
 }
---
+++ src/components/Connected.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Connected(props) {
+  return (
+    <div className="connected-container">
+      <h1 className="connected-header">You are connected to Metamask 🦊</h1>
+      <p className="connected-account">Metamask Account : {props.account}</p>
+    </div>
+  );
+}
+
+export default Connected;
